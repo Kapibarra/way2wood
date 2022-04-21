9ad2a5268a77f21efec433126fe2d30f29c53fbd
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-21 22:41:05 +0300

    generated all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './homepage/shared/components/header/header.component';
+import { HeroComponent } from './homepage/shared/components/hero/hero.component';
+import { AboutComponent } from './homepage/shared/components/about/about.component';
+import { CollectionComponent } from './homepage/shared/components/collection/collection.component';
+import { PriceComponent } from './homepage/shared/components/price/price.component';
+import { QuestionComponent } from './homepage/shared/components/question/question.component';
+import { DetailsComponent } from './homepage/shared/components/details/details.component';
+import { ContactsComponent } from './homepage/shared/components/contacts/contacts.component';
+import { FooterComponent } from './homepage/shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    CollectionComponent,
+    PriceComponent,
+    QuestionComponent,
+    DetailsComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
