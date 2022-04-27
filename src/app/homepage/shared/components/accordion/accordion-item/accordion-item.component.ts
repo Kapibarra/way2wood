702a536a86217a768b94378d7a6b750dea4ac8d0
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-27 19:46:24 +0300

    accordion animations

--- src/app/homepage/shared/components/accordion/accordion-item/accordion-item.component.ts
+++ src/app/homepage/shared/components/accordion/accordion-item/accordion-item.component.ts
@@ -1,9 +1,33 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion-item',
   templateUrl: './accordion-item.component.html',
-  styleUrls: ['./accordion-item.component.scss']
+  styleUrls: ['./accordion-item.component.scss'],
+  animations:[
+    trigger('smoothCollapse', [
+      state('initial' ,style({
+        height: '0',
+        overflow: 'hidden',
+        opacity: '0',
+        visibility: 'hidden'
+      })),
+      state('final',style({
+        overflow: 'hidden',
+      })),
+      transition('initial<=>final', animate('250ms'))
+    ]),
+    trigger('rotatedState', [
+      state('default' ,style({
+transform: 'rotate(0)'
+      })),
+      state('rotated' ,style({
+        transform: 'rotate(45deg)'
+      })),
+      transition('default<=>rotated', animate('250ms'))
+    ])
+  ]
 })
 export class AccordionItemComponent implements OnInit {
 
