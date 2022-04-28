2f1930c6c267428a3280b6ed4cdd052d2aec7d2f
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-28 15:30:52 +0300

    ornament collection

--- src/app/homepage/shared/components/collection/collection.component.ts
+++ src/app/homepage/shared/components/collection/collection.component.ts
@@ -25,9 +25,6 @@ interface Collection {
 export class CollectionComponent implements OnInit {
   Flora: Collection[] = [
     {
-      src: 'assets/img/flora/flora02.png',
-    },
-    {
       src: 'assets/img/flora/flora01.png',
     },
     {
@@ -36,13 +33,14 @@ export class CollectionComponent implements OnInit {
     {
       src: 'assets/img/flora/flora04.png',
     },
-
-
     {
       src: 'assets/img/flora/flora05.png',
     },
     {
       src: 'assets/img/flora/flora06.png',
+    },
+    {
+      src: 'assets/img/flora/flora02.png',
     },
     {
       src: 'assets/img/flora/flora07.png',
@@ -53,26 +51,18 @@ export class CollectionComponent implements OnInit {
     {
       src: 'assets/img/ornament/ornament01.png',
     },
-    {
-      src: 'assets/img/ornament/ornament02.png',
-    },
+
     {
       src: 'assets/img/ornament/ornament03.png',
     },
     {
+      src: 'assets/img/ornament/ornament02.png',
+    },
+    {
       src: 'assets/img/ornament/ornament04.png',
     },
-    {
-      src: 'assets/img/ornament/ornament05.png',
-    },
-    {
-      src: 'assets/img/ornament/ornament06.png',
-    },
-    {
-      src: 'assets/img/ornament/ornament07.png',
-    },
   ]
-  activeTab: string = 'Forest';
+  activeTab: string = 'Flora';
   constructor() {}
 
   ngOnInit(): void {}
