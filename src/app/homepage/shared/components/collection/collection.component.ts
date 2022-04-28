95058b9388befcb12a81c9307a4bb60bc84945c9
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-28 13:24:57 +0300

    added animation

--- src/app/homepage/shared/components/collection/collection.component.ts
+++ src/app/homepage/shared/components/collection/collection.component.ts
@@ -1,8 +1,20 @@
+import { style, transition, trigger, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('250ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('250ms', style({ opacity: 0 }))
+      ])
+    ]),
+  ],
   styleUrls: ['./collection.component.scss'],
 })
 export class CollectionComponent implements OnInit {
