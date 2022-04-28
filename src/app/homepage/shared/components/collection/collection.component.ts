17e7a532a8767bc680f148b77d2820c5ad72ccf6
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-28 14:16:44 +0300

    flora collection ngFor

--- src/app/homepage/shared/components/collection/collection.component.ts
+++ src/app/homepage/shared/components/collection/collection.component.ts
@@ -1,6 +1,11 @@
 import { style, transition, trigger, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
+interface Collection {
+  src: string;
+  id? : number,
+}
+
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
@@ -18,6 +23,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collection.component.scss'],
 })
 export class CollectionComponent implements OnInit {
+  Flora: Collection[] = [
+    {
+      src: 'assets/img/flora/flora02.png',
+    },
+    {
+      src: 'assets/img/flora/flora01.png',
+    },
+    {
+      src: 'assets/img/flora/flora03.png',
+    },
+    {
+      src: 'assets/img/flora/flora04.png',
+    },
+
+
+    {
+      src: 'assets/img/flora/flora05.png',
+    },
+    {
+      src: 'assets/img/flora/flora06.png',
+    },
+    {
+      src: 'assets/img/flora/flora07.png',
+    },
+  ]
+  Forest: Collection[] = []
+  Ornament: Collection[] = [
+    {
+      src: 'assets/img/ornament/ornament01.png',
+    },
+    {
+      src: 'assets/img/ornament/ornament02.png',
+    },
+    {
+      src: 'assets/img/ornament/ornament03.png',
+    },
+    {
+      src: 'assets/img/ornament/ornament04.png',
+    },
+    {
+      src: 'assets/img/ornament/ornament05.png',
+    },
+    {
+      src: 'assets/img/ornament/ornament06.png',
+    },
+    {
+      src: 'assets/img/ornament/ornament07.png',
+    },
+  ]
   activeTab: string = 'Forest';
   constructor() {}
 
