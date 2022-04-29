8c7cceddfbb33c7ae3c05feb092ea33964632636
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-29 12:32:48 +0300

    fixes

--- src/app/homepage/shared/components/price/price.component.ts
+++ src/app/homepage/shared/components/price/price.component.ts
@@ -1,15 +1,29 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-price',
   templateUrl: './price.component.html',
+  animations: [
+    trigger('Opacity', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('250ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('250ms', style({ opacity: 0 }))
+      ])
+    ]),
+  ],
   styleUrls: ['./price.component.scss']
 })
 export class PriceComponent implements OnInit {
-
+  activeTab: string = 'Flora';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onTabCLick(tab: any) {
+    this.activeTab = tab;
+  }
 }
