b9231dfbde35b9093acfc079ce149ac08f54ff8d
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-05-04 14:16:22 +0300

    fix mailer php

--- src/assets/handler.php
+++ src/assets/handler.php
@@ -10,14 +10,13 @@ require 'php/PHPMailer-5.2.28/src/SMTP.php';
 $mail = new PHPMailer(true);
 $mail_subject = 'Заявка с сайта';
 $mail_to_email = 'El-ram@bk.ru'; // your email
-$mail_to_name = 'Electro-hand.ru';
+$mail_to_name = 'Way2Wood.ru';
 
 try {
 
 	$mail_from_name = isset( $_POST['name'] ) ? $_POST['name'] : '';
 	$mail_from_phone = isset( $_POST['phone'] ) ? $_POST['phone'] : '';
-	$mail_from_service = isset( $_POST['service'] ) ? $_POST['service'] : '';
-	$mail_from_square = isset( $_POST['square'] ) ? $_POST['square'] : '';
+	$mail_from_service = isset( $_POST['message'] ) ? $_POST['service'] : '';
 	
 	
 
@@ -26,11 +25,11 @@ try {
 	$mail->Host = 'smtp.yandex.com'; // Set the SMTP server to send through
 	$mail->SMTPAuth = true; // Enable SMTP authentication
 	$mail->Username = 'noviweb@yandex.ru'; // SMTP username
-	$mail->Password = 'cf911313'; // SMTP password
+	$mail->Password = 'Cf911313'; // SMTP password
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-	$mail->setFrom($mail->Username, 'Electro-hand.ru'); // Your email
+	$mail->setFrom($mail->Username, 'Way2Wood.ru'); // Your email
 	$mail->addAddress($mail_to_email, $mail_to_name); // Add a recipient на какой емейл отправить!! проверить!!!!
 
 	for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
@@ -44,9 +43,9 @@ try {
 	$mail->Body = '
 		
 		<strong>Phone:</strong> ' . $mail_from_phone . '<br>
-		<strong>Service:</strong> ' . $mail_from_service . '<br>
-		<strong>Square:</strong> ' . $mail_from_square . '<br>
-		<strong>Name:</strong> ' . $mail_from_name; 
+		<strong>Name:</strong> ' . $mail_from_name . '<br>
+		<strong>Message:</strong> ' . $mail_from_service;
+
 		
 	$mail->Send();
 
