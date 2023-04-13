5520a92d9a806eb88e281e0c0557b48dbb9c9ab3
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-04-13 14:46:11 +0300

    new tabs

--- src/app/homepage/shared/components/collection/collection.component.ts
+++ src/app/homepage/shared/components/collection/collection.component.ts
@@ -2,29 +2,145 @@ import { style, transition, trigger, animate } from '@angular/animations';
 import { ViewportScroller } from '@angular/common';
 import { Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
 
-
 interface Collection {
   src: string;
-  id? : number,
+  id?: number;
 }
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
-  animations: [
-    trigger('Opacity', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('250ms', style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('250ms', style({ opacity: 0 }))
-      ])
-    ]),
-  ],
+  // animations: [
+  //   trigger('Opacity', [
+  //     transition(':enter', [
+  //       style({ opacity: 0 }),
+  //       animate('250ms', style({ opacity: 1 })),
+  //     ]),
+  //     transition(':leave', [animate('250ms', style({ opacity: 0 }))]),
+  //   ]),
+  // ],
   styleUrls: ['./collection.component.scss'],
 })
 export class CollectionComponent implements OnInit {
+  tabs = [
+    {
+      title: 'Flora',
+      active: true,
+      tabContent: [
+        {
+          src: 'assets/img/flora/flora01.png',
+        },
+        {
+          src: 'assets/img/flora/flora03.png',
+        },
+        {
+          src: 'assets/img/flora/flora05.png',
+        },
+        {
+          src: 'assets/img/flora/flora07.png',
+        },
+        {
+          src: 'assets/img/flora/flora04.png',
+        },
+
+        {
+          src: 'assets/img/flora/flora06.png',
+        },
+        {
+          src: 'assets/img/flora/flora09.png',
+        },
+        {
+          src: 'assets/img/flora/flora10.png',
+        },
+        {
+          src: 'assets/img/flora/flora11.png',
+        },
+        {
+          src: 'assets/img/flora/flora02.png',
+        },
+        {
+          src: 'assets/img/flora/flora08.png',
+        },
+        {
+          src: 'assets/img/flora/flora12.png',
+        },
+      ],
+    },
+    {
+      title: 'Forest',
+      active: false,
+      tabContent: [
+        {
+          src: 'assets/img/forest/forest_07.png',
+        },
+        {
+          src: 'assets/img/forest/forest_01.png',
+        },
+        {
+          src: 'assets/img/forest/forest_02.png',
+        },
+        {
+          src: 'assets/img/forest/forest_03.png',
+        },
+        {
+          src: 'assets/img/forest/forest_04.png',
+        },
+        {
+          src: 'assets/img/forest/forest_05.png',
+        },
+        {
+          src: 'assets/img/forest/forest_06.png',
+        },
+        {
+          src: 'assets/img/forest/forest_08.png',
+        },
+        {
+          src: 'assets/img/forest/forest_10.png',
+        },
+        {
+          src: 'assets/img/forest/forest_09.png',
+        },
+      ],
+    },
+    {
+      title: 'Ornament',
+      active: false,
+      tabContent: [
+        {
+          src: 'assets/img/ornament/ornament01.png',
+        },
+
+        {
+          src: 'assets/img/ornament/ornament03.png',
+        },
+
+        {
+          src: 'assets/img/ornament/ornament05.png',
+        },
+        {
+          src: 'assets/img/ornament/ornament02.png',
+        },
+        {
+          src: 'assets/img/ornament/ornament07.png',
+        },
+        {
+          src: 'assets/img/ornament/ornament08.png',
+        },
+        {
+          src: 'assets/img/ornament/ornament04.png',
+        },
+        {
+          src: 'assets/img/ornament/ornament09.png',
+        },
+        {
+          src: 'assets/img/ornament/ornament10.png',
+        },
+        {
+          src: 'assets/img/ornament/ornament11.png',
+        },
+      ],
+    },
+  ];
   Flora: Collection[] = [
     {
       src: 'assets/img/flora/flora01.png',
@@ -63,7 +179,7 @@ export class CollectionComponent implements OnInit {
     {
       src: 'assets/img/flora/flora12.png',
     },
-  ]
+  ];
   Forest: Collection[] = [
     {
       src: 'assets/img/forest/forest_07.png',
@@ -95,8 +211,7 @@ export class CollectionComponent implements OnInit {
     {
       src: 'assets/img/forest/forest_09.png',
     },
-
-  ]
+  ];
   Ornament: Collection[] = [
     {
       src: 'assets/img/ornament/ornament01.png',
@@ -106,7 +221,6 @@ export class CollectionComponent implements OnInit {
       src: 'assets/img/ornament/ornament03.png',
     },
 
-    
     {
       src: 'assets/img/ornament/ornament05.png',
     },
@@ -131,16 +245,15 @@ export class CollectionComponent implements OnInit {
     {
       src: 'assets/img/ornament/ornament11.png',
     },
-  ]
+  ];
   activeTab: string = 'Flora';
 
-  constructor(private viewportscroller: ViewportScroller,) {}
+  constructor(private viewportscroller: ViewportScroller) {}
 
-  @ViewChildren("imageEl")
+  @ViewChildren('imageEl')
   imagesRef!: ElementRef<HTMLImageElement>[];
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onTabCLick(tab: any) {
     this.activeTab = tab;
@@ -148,6 +261,10 @@ export class CollectionComponent implements OnInit {
   onClickScroll(elementId: string): void {
     this.viewportscroller.scrollToAnchor(elementId);
   }
-
-
+  selectTab(tab: { active: boolean }) {
+    this.tabs.forEach((tab) => {
+      tab.active = false;
+    });
+    tab.active = true;
+  }
 }
